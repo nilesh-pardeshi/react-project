9e41bc7ffb2e899cceaf2cf8270568c970728e2d
Nilesh Pardeshi <nileshpardeshi2645@gmail.com>
2025-06-22 12:32:30 +0530

    we created Lottery Game for 3 digit number

---
+++ LotteryGame/src/App.jsx
@@ -0,0 +1,24 @@
+import TicketNum from './TicketNum'
+import './App.css'
+import Ticket from './Ticket'
+import Lottery from './Lotteryl'
+import { sum } from './helper'
+
+function App() {
+
+  let winCondition=(ticket)=>{
+      return sum(ticket)===15;
+      //return ticket.every((num) => num===ticket[0]);
+      //return ticket[0]===0;
+      
+  };
+
+  return (
+    <>
+      <Lottery n={3} winCondition={winCondition}/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ LotteryGame/src/Button.jsx
@@ -0,0 +1,4 @@
+import './Button.css';
+export default function Button({action}){
+    return <button onClick={action}>Buy New Ticket</button>;
+}
