157caf864fbfd90eb99c1499bf221fe3db05107f
Nilesh Pardeshi <nileshpardeshi2645@gmail.com>
2025-07-09 16:45:26 +0530

    we access weather api

---
+++ WeatherApp/src/App.jsx
@@ -0,0 +1,14 @@
+import Form from './Form'
+import './App.css'
+import SearchBox from './SearchBox'
+function App() {
+
+
+  return (
+    <>
+      <SearchBox/>
+    </>
+  )
+}
+
+export default App
